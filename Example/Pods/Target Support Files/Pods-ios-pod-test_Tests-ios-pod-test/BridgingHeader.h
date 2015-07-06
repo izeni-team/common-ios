c9eb149cdb0433444619c935e1f10309fe8c63e2
Taylor Allred <tallred@izeni.com>
2015-07-06 14:47:45 -0600

    Added bridging header.

---
+++ Example/Pods/Target Support Files/Pods-ios-pod-test_Tests-ios-pod-test/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  Pods
+//
+//  Created by Taylor Allred on 7/6/15.
+//
+//
+
+#ifndef Pods_BridgingHeader_h
+#define Pods_BridgingHeader_h
+
+#import "DistanceFormatter.h"
+
+#endif
