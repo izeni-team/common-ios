691658a787ad451dd6b49693db8f8764558cfbfe
Taylor Allred <tallred@izeni.com>
2015-07-06 15:22:30 -0600

    Added Reachability.

---
+++ Pod/Classes/Reachability.swift
@@ -0,0 +1,32 @@
+//
+//  Reachability.swift
+//  Brower
+//
+//  Created by Skyler Smith on 6/4/15.
+//  Copyright (c) 2015 Izeni. All rights reserved.
+//
+
+import Foundation
+import SCNetworkReachability
+import EDQueue
+
+class Reachability {
+    private static var singleton: Reachability!
+    var watcher: SCNetworkReachability!
+    static var isReachable = true
+    private static let defaultHost = "http://8.8.8.8"
+    static let changedBroadcast = NSUUID()
+    
+    class func initialize() {
+        singleton = Reachability()
+        singleton.watcher = SCNetworkReachability(host: defaultHost)
+        
+        singleton.watcher.observeReachability { status in
+            let wasReachable = Reachability.isReachable
+            Reachability.isReachable = status != .NotReachable
+            if Reachability.isReachable != wasReachable {
+                Broadcast.emit(Reachability.changedBroadcast, value: NSNumber(bool: Reachability.isReachable))
+            }
+        }
+    }
+}
