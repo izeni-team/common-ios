d2f2c6b931d085db9353fc28593ddfafd4e944bc
Jacob Ovard <jovard@izeni.com>
2015-09-02 11:38:43 -0600

    Added alerts to alert user if app doesn't have camera permissions.

--- Pod/Classes/ImagePicker.swift
+++ Pod/Classes/ImagePicker.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import PEPhotoCropEditor
+import AVFoundation
 
 @objc public protocol ImagePickerDelegate: class {
     /**
@@ -31,9 +32,24 @@ public class ImagePicker: NSObject, UIImagePickerControllerDelegate, PECropViewC
     private var aspectRatio: CGFloat?
     public var popoverSource: UIView! // Required for iPad
     public static var singleton = ImagePicker()
-    
+    public static var allowChoosingFromLibrary = true
+    public static var allowTakingPhoto = true
+    public static var takePhotoTitle = "Take Photo"
+    public static var chooseFromLibraryTitle = "Choose from Library"
     public class var isLibraryAvailable: Bool {
         return UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary)
+    }
+    
+    /**
+    Opens a popover from which the user may select an image from their photo library or take a new picture.
+    Cropping aspect ratio defaults to a square.
+    
+    :param: from The UIViewController that will present the popover alert.
+    :param: popoverSource For iPad: The UIView that defines where the popover will be placed.
+    :param: delegate the ImagePickerDelegate that will receive calls to imagePicked() and, if implemeneted, imagePickCancelled().
+    */
+    public class func pickImage(#from: UIViewController, popoverSource: UIView, delegate: ImagePickerDelegate) {
+        _pickImage(from: from, popoverSource: popoverSource, delegate: delegate, aspectRatio: 1) // Defaults to a square
     }
     
     /**
@@ -42,39 +58,61 @@ public class ImagePicker: NSObject, UIImagePickerControllerDelegate, PECropViewC
     :param: from The UIViewController that will present the popover alert.
     :param: popoverSource For iPad: The UIView that defines where the popover will be placed.
     :param: delegate the ImagePickerDelegate that will receive calls to imagePicked() and, if implemeneted, imagePickCancelled().
+    :param: aspectRatio the aspect ratio you wish to lock cropping to. Specify 1 for a square, nil to let the user decide.
     */
-    class func pickImage(#from: UIViewController, popoverSource: UIView, delegate: ImagePickerDelegate, withAspectRatio aspectRatio: CGFloat? = nil) {
-        
-        assert(contains(NSBundle.allFrameworks().map { $0.bundleURL.lastPathComponent! }, "PEPhotoCropEditor.framework"), "Your project does not contain the PEPhotoCropEditor bundle. Try creating a reference to it in your main project. You can do this by adding the PEPhotoCropEditor.bundle to your main project and uncheck the \"copy\" box.")
-        
-        singleton.delegate = delegate
-        singleton.parentVC = from
-        singleton.popoverSource = popoverSource
-        
-        if let ar = aspectRatio {
-            singleton.aspectRatio = ar
-        }
-        
-        let alert = UIAlertController()
-        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
-            alert.addAction(UIAlertAction(title: "Take Photo", style: .Default) { _ in
-                self.singleton.pickImage(.Camera)
-                })
-        }
-        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
-            alert.addAction(UIAlertAction(title: "Choose from Library", style: .Default) { _ in
-                self.singleton.pickImage(.PhotoLibrary)
-                })
-        }
-        if alert.actions.isEmpty {
-            let alert = UIAlertController(title: "Photos Unavailable", message: "Your device does not appear to have either a camera or photo library.", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { _ in }))
-            from.presentViewController(alert, animated: true, completion: nil)
-        }
-        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { _ in }))
-        singleton.show(alert)
+    public class func pickImage(#from: UIViewController, popoverSource: UIView, delegate: ImagePickerDelegate, aspectRatio: CGFloat?) {
+        _pickImage(from: from, popoverSource: popoverSource, delegate: delegate, aspectRatio: aspectRatio)
     }
     
+    class func _pickImage(#from: UIViewController, popoverSource: UIView, delegate: ImagePickerDelegate, aspectRatio: CGFloat?) {
+        assert(contains(NSBundle.allFrameworks().map { $0.bundleURL.lastPathComponent! }, "PEPhotoCropEditor.framework"), "Your project does not contain the PEPhotoCropEditor bundle. Try creating a reference to it in your main project. You can do this by adding the PEPhotoCropEditor.bundle to your main project and uncheck the \"copy\" box.")
+        
+        singleton.parentVC = from
+        singleton.popoverSource = popoverSource
+        singleton.delegate = delegate
+        singleton.aspectRatio = aspectRatio
+
+        let appName = NSBundle.mainBundle().infoDictionary!["CFBundleName"] as! String
+        let status = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
+        
+        switch status {
+        case .Restricted:
+            let authAlert = UIAlertController(title: "Camera is Restricted", message: "\(appName) could not access the camera on this device.", preferredStyle: .Alert)
+            authAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { _ in
+            }))
+            self.singleton.show(authAlert)
+        case .Denied:
+            let authAlert = UIAlertController(title: "Camera Access is Disabled", message: "\(appName) does not have access to your camera. You can enable access in privacy settings.", preferredStyle: .Alert)
+            authAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { _ in
+            }))
+            self.singleton.show(authAlert)
+        case AVAuthorizationStatus.NotDetermined:
+            let authAlert = UIAlertController(title: "\(appName) Would Like to Access Your Camera", message: "\(appName) does not have access to your camera. You can enable access in privacy settings.", preferredStyle: .Alert)
+            authAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { _ in
+            }))
+            self.singleton.show(authAlert)
+        case AVAuthorizationStatus.Authorized:
+            let alert = UIAlertController()
+            if allowTakingPhoto && UIImagePickerController.isSourceTypeAvailable(.Camera) {
+                alert.addAction(UIAlertAction(title: takePhotoTitle, style: .Default) { _ in
+                    self.singleton.pickImage(.Camera)
+                    })
+            }
+            if allowChoosingFromLibrary && UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+                alert.addAction(UIAlertAction(title: chooseFromLibraryTitle, style: .Default) { _ in
+                    self.singleton.pickImage(.PhotoLibrary)
+                    })
+            }
+            if alert.actions.isEmpty {
+                let alert = UIAlertController(title: "Photos Unavailable", message: "Your device does not appear to have either a camera or photo library.", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { _ in }))
+                from.presentViewController(alert, animated: true, completion: nil)
+            }
+            alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { _ in }))
+            singleton.show(alert)
+        }
+    }
+
     public func show(vc: UIViewController) {
         if iPad {
             let popover = UIPopoverController(contentViewController: vc)
