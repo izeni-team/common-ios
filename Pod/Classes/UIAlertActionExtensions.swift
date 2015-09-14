d664100f5ce3e47f4945dfa20d6566567cd3c77d
Bryan Henderson <bhenderson@izeni.com>
2015-09-14 11:24:11 -0600

    Added UIAlertController and UIAlertAction convenience methods.

---
+++ Pod/Classes/UIAlertActionExtensions.swift
@@ -0,0 +1,39 @@
+//
+//  UIAlertActionExtensions.swift
+//  Izeni
+//
+//  Created by Christopher Bryan Henderson on 9/14/15.
+//  Copyright (c) 2015 Izeni. All rights reserved.
+//
+
+import UIKit
+
+public extension UIAlertAction {
+    public class func cancelAction() -> UIAlertAction {
+        return UIAlertAction(title: "Cancel", callback: nil)
+    }
+    
+    public class func cancelAction(callback: (() -> Void)?) -> UIAlertAction {
+        return UIAlertAction(title: "Cancel", callback: callback)
+    }
+    
+    public class func okAction() -> UIAlertAction {
+        return UIAlertAction(title: "OK", callback: nil)
+    }
+    
+    public class func okAction(callback: (() -> Void)?) -> UIAlertAction {
+        return UIAlertAction(title: "OK", callback: callback)
+    }
+    
+    public convenience init(title: String, callback: (() -> Void)?) {
+        self.init(title: title, style: .Default, handler: { _ in
+            callback?()
+        })
+    }
+    
+    public convenience init(destructiveTitle: String, callback: (() -> Void)?) {
+        self.init(title: destructiveTitle, style: .Destructive, handler: { _ in
+            callback?()
+        })
+    }
+}
