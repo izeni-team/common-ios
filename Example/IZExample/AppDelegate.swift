62e13faee3b609470bae3548696618de746eca81
Bryan Henderson <bhenderson@izeni.com>
2015-11-13 14:36:35 -0700

    Added code to ask for notification permission.

--- Example/IZExample/AppDelegate.swift
+++ Example/IZExample/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let types: UIUserNotificationType = [.Badge, .Sound, .Alert]
+        let settings = UIUserNotificationSettings(forTypes: types, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        
         return true
     }
 
