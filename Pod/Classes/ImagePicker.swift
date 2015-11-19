5106a52327ed3cd4a6f181265fac58dd2bee1b32
Jacob Ovard <jovard@izeni.com>
2015-11-19 14:41:16 -0700

    Updated ImagePicker warning and alerts for user senarios.

--- Pod/Classes/ImagePicker.swift
+++ Pod/Classes/ImagePicker.swift
@@ -12,15 +12,15 @@ import AVFoundation
 
 @objc public protocol ImagePickerDelegate: class {
     /**
-    imagePicked() is called when the user has finished selecting and cropping an image.
-    
-    - parameter image: The resulting image after being cropped.
-    */
+     imagePicked() is called when the user has finished selecting and cropping an image.
+     
+     - parameter image: The resulting image after being cropped.
+     */
     func imagePicked(image: UIImage)
     
     /**
-    imagePickCancelled() is called when the user taps 'cancel' and closes the image picker.
-    */
+     imagePickCancelled() is called when the user taps 'cancel' and closes the image picker.
+     */
     optional func imagePickCancelled()
 }
 
@@ -41,25 +41,25 @@ public class ImagePicker: NSObject, UIImagePickerControllerDelegate, PECropViewC
     }
     
     /**
-    Opens a popover from which the user may select an image from their photo library or take a new picture.
-    Cropping aspect ratio defaults to a square.
-    
-    - parameter from: The UIViewController that will present the popover alert.
-    - parameter popoverSource: For iPad: The UIView that defines where the popover will be placed.
-    - parameter delegate: the ImagePickerDelegate that will receive calls to imagePicked() and, if implemeneted, imagePickCancelled().
-    */
+     Opens a popover from which the user may select an image from their photo library or take a new picture.
+     Cropping aspect ratio defaults to a square.
+     
+     - parameter from: The UIViewController that will present the popover alert.
+     - parameter popoverSource: For iPad: The UIView that defines where the popover will be placed.
+     - parameter delegate: the ImagePickerDelegate that will receive calls to imagePicked() and, if implemeneted, imagePickCancelled().
+     */
     public class func pickImage(from from: UIViewController, popoverSource: UIView, delegate: ImagePickerDelegate) {
         _pickImage(from: from, popoverSource: popoverSource, delegate: delegate, aspectRatio: 1, preferFrontCamera: false) // Defaults to a square
     }
     
     /**
-    Opens a popover from which the user may select an image from their photo library or take a new picture.
-    
-    - parameter from: The UIViewController that will present the popover alert.
-    - parameter popoverSource: For iPad: The UIView that defines where the popover will be placed.
-    - parameter delegate: the ImagePickerDelegate that will receive calls to imagePicked() and, if implemeneted, imagePickCancelled().
-    - parameter aspectRatio: the aspect ratio you wish to lock cropping to. Specify 1 for a square, nil to let the user decide.
-    */
+     Opens a popover from which the user may select an image from their photo library or take a new picture.
+     
+     - parameter from: The UIViewController that will present the popover alert.
+     - parameter popoverSource: For iPad: The UIView that defines where the popover will be placed.
+     - parameter delegate: the ImagePickerDelegate that will receive calls to imagePicked() and, if implemeneted, imagePickCancelled().
+     - parameter aspectRatio: the aspect ratio you wish to lock cropping to. Specify 1 for a square, nil to let the user decide.
+     */
     public class func pickImage(from from: UIViewController, popoverSource: UIView, delegate: ImagePickerDelegate, aspectRatio: CGFloat?) {
         _pickImage(from: from, popoverSource: popoverSource, delegate: delegate, aspectRatio: aspectRatio, preferFrontCamera: false)
     }
@@ -75,7 +75,7 @@ public class ImagePicker: NSObject, UIImagePickerControllerDelegate, PECropViewC
         singleton.popoverSource = popoverSource
         singleton.delegate = delegate
         singleton.aspectRatio = aspectRatio
-
+        
         let appName = NSBundle.mainBundle().infoDictionary!["CFBundleName"] as! String
         let status = AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)
         
@@ -89,41 +89,69 @@ public class ImagePicker: NSObject, UIImagePickerControllerDelegate, PECropViewC
             self.singleton.show(authAlert)
         case .Denied:
             let authAlert = UIAlertController(title: "Camera Access is Disabled", message: "\(appName) does not have access to your camera. You can enable access in privacy settings.", preferredStyle: .Alert)
+            
+            authAlert.addAction(UIAlertAction(title: "Settings", style: .Cancel, handler: { (action) -> Void in
+                UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
+            }))
+            
             authAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { _ in
             }))
+            
             self.singleton.show(authAlert)
         case AVAuthorizationStatus.NotDetermined:
             fallthrough
         case AVAuthorizationStatus.Authorized:
+            let cam = allowTakingPhoto
+            let camYes = UIImagePickerController.isSourceTypeAvailable(.Camera)
+            let lib = allowChoosingFromLibrary
+            let libYes = UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary)
+            
             let alert = UIAlertController()
-            if allowTakingPhoto && UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            
+            if cam && camYes {
                 alert.addAction(UIAlertAction(title: takePhotoTitle, style: .Default) { _ in
                     self.singleton.pickImage(.Camera, preferFrontCamera: preferFrontCamera)
                     })
             }
-            if allowChoosingFromLibrary && UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            if lib && libYes {
                 alert.addAction(UIAlertAction(title: chooseFromLibraryTitle, style: .Default) { _ in
                     self.singleton.pickImage(.PhotoLibrary, preferFrontCamera: preferFrontCamera)
                     })
             }
             if alert.actions.count == 1 {
-                if allowTakingPhoto {
+                if cam {
                     self.singleton.pickImage(.Camera, preferFrontCamera: preferFrontCamera)
                 } else {
                     self.singleton.pickImage(.PhotoLibrary, preferFrontCamera: preferFrontCamera)
                 }
-            } else {
-                if alert.actions.isEmpty {
-                    let alert = UIAlertController(title: "Photos Unavailable", message: "Your device does not appear to have either a camera or photo library.", preferredStyle: .Alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { _ in }))
-                    from.presentViewController(alert, animated: true, completion: nil)
-                }
+            } else if alert.actions.count == 2 {
                 alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { _ in }))
                 singleton.show(alert)
+            } else { //the following section is an exhaustive check to show the appropriate error message for all possibilities
+                if cam && !camYes && lib && !libYes {
+                    //SHOW error("Neither avaiable")
+                    let alert = UIAlertController(title: "Could not access Camera or Photo Library", message: "The Camera and Photo Library are currently unavaible.", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: { _ in }))
+                    from.presentViewController(alert, animated: true, completion: nil)
+                } else if cam && !camYes && !lib {
+                    //SHOW error("Camera not avaiable")
+                    let alert = UIAlertController(title: "Could not access Camera", message: "The Camera is currently unavailable.", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: { _ in }))
+                    from.presentViewController(alert, animated: true, completion: nil)
+                } else if !cam && lib && !libYes {
+                    //SHOW error("Library not avaiable")
+                    let alert = UIAlertController(title: "Could not access Photo Library", message: "The Photo Library is currently unavailable.", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: { _ in }))
+                    from.presentViewController(alert, animated: true, completion: nil)
+                } else {
+                    //SHOW TO CONSOLE - developer error (print error to console, then do nothing--return early) - developer has not allows camera or library
+                    print("*****ERROR: SELECTING MEDIA FROM USER HAS BEEN DISABLED - Developer has choosen not to allow user to select images or video from their camera or photo library.")
+                    return
+                }
             }
         }
     }
-
+    
     public func show(vc: UIViewController) {
         if iPad {
             let popover = UIPopoverController(contentViewController: vc)
@@ -138,7 +166,7 @@ public class ImagePicker: NSObject, UIImagePickerControllerDelegate, PECropViewC
         picker.delegate = self
         picker.sourceType = type
         if preferFrontCamera && picker.sourceType == .Camera && UIImagePickerController.isCameraDeviceAvailable(.Front) {
-            picker.cameraDevice = UIImagePickerControllerCameraDevice.Front;
+            picker.cameraDevice = UIImagePickerControllerCameraDevice.Front
         }
         show(picker)
     }
