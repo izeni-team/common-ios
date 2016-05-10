be3802b4f3253dd94d12a1bb2d2740c57d515dd9
Taylor Allred <tallred@izeni.com>
2016-05-10 14:42:12 -0600

    reworked the network file

--- Pod/Classes/IzeniNetwork.swift
+++ Pod/Classes/IzeniNetwork.swift
@@ -10,19 +10,31 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
-// Subclass to change host, headers, etc.
+public struct JSONRequestFailureResponse {
+    let status: Int?
+    let result: JSON?
+    let message: String
+    
+    func showAlert(vc: UIViewController, title: String = "Error") {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(.okAction())
+        vc.presentViewController(alert, animated: true, completion: nil)
+    }
+}
+
+public enum JSONRequestResponse {
+    case Success(result: JSON)
+    case Failure(result: JSONRequestFailureResponse)
+}
+
 // TODO: Add multipart upload
 public class IzeniNetwork {
     public struct Broadcasts {
         public static let gotHTTP401 = NSUUID()
     }
     
-    public class func getApiHost() -> String {
-        return Preferences.get(.apiHost)!
-    }
-    
     // Subclass and override to change
-    public static func getJSONHeaders() -> [String:String] {
+    public class func getJSONHeaders() -> [String:String] {
         var headers = [String:String]()
         headers["Content-Type"] = "application/json"
         if let token: String = Preferences.get(.loginToken) {
@@ -31,48 +43,77 @@ public class IzeniNetwork {
         return headers
     }
     
-    private static var manager: Manager?
-
-    public class func createManager() -> Manager {
-        return Manager.sharedInstance
-    }
+    public static var disableSSL = false
+    public static var serverURL = "https://www.google.com/"
     
-    private class func getManager() -> Manager {
-        if manager == nil {
-            manager = createManager()
+    public static let manager: Manager = {
+        if IzeniNetwork.disableSSL {
+            let url = NSURL(string: IzeniNetwork.serverURL)!
+            let policies = [
+                url.host!: ServerTrustPolicy.DisableEvaluation
+            ]
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            config.HTTPAdditionalHeaders = Alamofire.Manager.defaultHTTPHeaders
+            return Manager(configuration: config, serverTrustPolicyManager: ServerTrustPolicyManager(policies: policies))
+        } else {
+            return Manager.sharedInstance
         }
-        return manager!
-    }
+    }()
     
     public typealias Method = Alamofire.Method
     
-    public class func makeRequest(method: Method, endpoint: String, json: JSON?, success: (json: JSON?) -> Void, failure: (status: Int?, json: JSON?) -> Void) -> NSURLSessionTask {
-        print(method.rawValue + " " + endpoint)
+    public class func makeRequest(method: Method, endpoint: String, json: JSON?, completion: (result: JSONRequestResponse) -> Void) -> NSURLSessionTask {
+        print("<--- \(method.rawValue) \(endpoint)")
         
-        let requestManager = getManager()
-        let request = requestManager.request(method, getApiHost() + endpoint, parameters: json?.dictionaryObject, encoding: .JSON, headers: getJSONHeaders())
+        let request = manager.request(method, serverURL + endpoint, parameters: json?.dictionaryObject, encoding: .JSON, headers: getJSONHeaders())
         request.responseJSON { response in
+            if response.result.error?.code == NSURLErrorCancelled {
+                return
+            }
+            let failedConnection = "Failed to connect to the server. Please check you Internet Connection and try again."
+            let unknown = "An unknown error occcured."
+            let serverError = "The server appears to be experiencing a temporary glitch. If the problem persists, please contact support."
+            
+            guard let status = response.response?.statusCode else {
+                completion(result: .Failure(result: JSONRequestFailureResponse(status: nil, result: nil, message: failedConnection)))
+                return
+            }
+            
+            print("---> \(status) \(endpoint)")
             var jsonResponse: JSON?
-            print("\(response.response?.statusCode ?? 0) \(endpoint)")
+            
             if let value = response.result.value where value is [AnyObject] || value is [String:AnyObject] {
                 print(jsonResponse)
                 jsonResponse = JSON(value)
             } else if let error = response.result.error {
                 print(error)
-            } else {
             }
             
-            switch response.response?.statusCode {
-            case .Some(let status) where (200..<300).contains(status):
-                success(json: jsonResponse)
-            case let status where status == 401:
-                Broadcast.emit(Broadcasts.gotHTTP401)
-                fallthrough
+            let error = { (message: String) -> Void in
+                completion(result: .Failure(result: JSONRequestFailureResponse(status: status, result: jsonResponse, message: message)))
+            }
+            
+            switch status {
+            case 200..<300:
+                completion(result: .Success(result: jsonResponse ?? JSON(NSNull())))
+            case 401:
+                completion(result: .Failure(result: JSONRequestFailureResponse(status: status, result: jsonResponse, message: "Unauthorized.")))
+            case 400...499:
+                if let field = jsonResponse?.dictionaryValue.keys.first, message = jsonResponse?[field].arrayValue.first?.string {
+                    error(field + ": " + message)
+                } else if let responseDictionary = response.result.value as? [String: AnyObject], message = responseDictionary["detail"] as? String {
+                    error(message)
+                } else if let message = jsonResponse?.arrayValue.first?.string {
+                    error(message)
+                } else {
+                    error(unknown)
+                }
+            case 500...599:
+                error(serverError)
             default:
-                failure(status: response.response?.statusCode, json: jsonResponse)
+                error(unknown)
             }
         }
         return request.task
     }
-    
 }
